8cdc71fb7282ac7977b7425d1b9bad5f16234c1a
Gabriel Abreu Oliveira de Andrade <gabriel.oandrade@b2wdigital.com>
2024-02-21 16:45:45 -0300

    feat: Swagger configured  to generate documentation

--- src/main/java/com/smartqueue/order/api/item/controller/ItemController.java
+++ src/main/java/com/smartqueue/order/api/item/controller/ItemController.java
@@ -1,15 +1,21 @@
 package com.smartqueue.order.api.item.controller;
 
 import com.smartqueue.order.api.item.representation.ItemResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Item", description = "Resource for items management")
 public interface ItemController {
 
-    ResponseEntity<Page<ItemResponse>> list(@PageableDefault(
+
+    @Operation(description = "Returns list of items")
+    ResponseEntity<Page<ItemResponse>> list(@ParameterObject @PageableDefault(
             page = 0,
             size = 20,
             sort = "id",
