7ae312cae15b2781500aa329bd771cbd03f3758e
Gabriel Abreu Oliveira de Andrade <gabriel.oandrade@b2wdigital.com>
2024-02-21 17:02:25 -0300

    refactor: OrderRequest items object

--- src/main/java/com/smartqueue/order/api/order/representation/OrderRequest.java
+++ src/main/java/com/smartqueue/order/api/order/representation/OrderRequest.java
@@ -1,18 +1,20 @@
 package com.smartqueue.order.api.order.representation;
 
-import com.smartqueue.order.domain.item.model.Item;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequest {
 
-    private Map<Long, String> items;
+    private List<OrderItem> items;
 
+    public record OrderItem(@Schema(description = "Number of items", example = "4") Long units,
+                            @Schema(description = "Item Id", example = "65d552bc3c175abc76126308") String itemId) {
+    }
 }
